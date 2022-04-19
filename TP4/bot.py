a76194287103eeb523de57a9dc73b844e4fc8bb0
MAILLOT Chloé <cmaillot@et.esiea.fr>
2022-04-19 22:17:39 +0200

    Command class and help message

---
+++ TP4/bot.py
@@ -0,0 +1,29 @@
+from discord.ext import commands
+import json
+import logging
+
+
+class MyBot(commands.Bot) :
+    
+    def __init__(self) :
+        super().__init__(command_prefix="!")
+        self.remove_command("help")
+        with open('config.json', 'r') as json_file:
+            json_load = json.load(json_file)
+        self.token = json_load["token"]
+        
+        
+    async def on_ready(self):
+        bot.load_extension("cogs.commandes")
+        bot_channel = self.get_channel(961242684318683136)
+        await bot_channel.send(f"JamesThePyBot is ready to help you !")
+        logging.info("JamesThePyBot is ready to help you !")
+
+
+if __name__ == "__main__" :
+    
+    logging.basicConfig(filename="logs.log", level=logging.INFO, format="%(asctime)s - %(levelname)s: %(message)s")         #format how will the data be displayed in the file
+    
+    
+    bot = MyBot()
+    bot.run(bot.token)
---
+++ TP4/cogs/commandes.py
@@ -0,0 +1,63 @@
+import discord
+from discord.ext import commands
+from shiritori import shiritori
+
+
+class MyCommands(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        
+        
+    @commands.command()
+    async def help(self, ctx):
+        embed=discord.Embed(title="What can James do for you ?", url="", description="This is a manual on how to use James", color=0xFF5733)
+        
+        embed.set_author(name="Chloé", url="https://github.com/kur03")
+        
+        embed.add_field(name="Real commands", value="These are the commands you can use with the prefix !.", inline=False)
+        embed.add_field(name="!help", value="Sends this message.", inline=True)
+        embed.add_field(name="!del", value="As you guessed it ~~adds~~ delete the message. Add the number of message.", inline=True)
+        
+        embed.add_field(name="Not real commands", value="These are the commands you can use with the prefix !.", inline=False)
+        embed.add_field(name="gperdu", value="Display a little 'I Lost' picture.", inline=True)
+        
+        embed.add_field(name="Games", value="These are the commands you can use with the prefix !.", inline=False)
+        embed.add_field(name="shiritoi", value="Check and save words used in the shiritori.", inline=True)
+        
+        embed.set_footer(text="For any question @kuroe#4981")
+        
+        await ctx.send(embed=embed)
+        
+        
+    @commands.command(name="del")
+    async def delete(self, ctx, number: int):
+        messages = await ctx.channel.history(limit=number + 1).flatten()
+        
+        for each_message in messages:
+            await each_message.delete()
+    
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.content == "gperdu":
+            #Pour envoyer un message avec le bot, on utilise message.channel 
+            # qui nous permet de récupérer le salon sur lequel le message a été envoyé. 
+            # On utilise ensuite la méthode send de ce salon pour envoyer un message avec le bot.
+            await message.channel.send("https://cdn.discordapp.com/attachments/760200285498376212/958278241314025532/sipersicret.png")           
+
+        if message.channel.name == "🐍-tp4" :
+            if message.author.name != "JamesThePyBot" :    
+                if shiritori(message.content) :
+                    await message.channel.send(f"{message.author.mention} ce mot a déjà été utilisé tu as donc perdu. Tu pourras recommencer à jouer au prochain tour !")
+                    
+           
+              
+    async def on_member_join(self, member):
+        general_channel = self.client.get_channel(959413124648271979)
+        print(f"L'utilisateur {member.display_name} a rejoint le serveur !")
+        await general_channel.send(f"We've got a lost tourist here !")
+    
+        
+        
+def setup(bot):
+    bot.add_cog(MyCommands(bot))
